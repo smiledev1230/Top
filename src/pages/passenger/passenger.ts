5f7468c58c79bc3318def52411f2d15ccc122a41
Web <30370727+smile-monkey@users.noreply.github.com>
2018-02-25 11:59:34 +0700

    passenger page style

--- src/pages/passenger/passenger.ts
+++ src/pages/passenger/passenger.ts
@@ -6,12 +6,28 @@ import { KeepPostedPage } from '../keep-posted/keep-posted';
   templateUrl: 'passenger.html',
 })
 export class PassengerPage {
-
+  private passengers: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.passengers = [
+      'Boost my profile',
+      'Optimize my best photos',
+      'Get extra undo to change your mind',
+      'Read receipts',
+      'Get unlimited supertop swips',
+      'Get on top search when in your destination'
+    ]
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PassengerPage');
+  }
+
+  passengerSelected(passenger) {
+
+  }
+
+  payNow() {
+    this.navCtrl.push(KeepPostedPage);
   }
 
   nextPage() {
